beb58ccd8f4aca50828ad1a811f77bf5820a5cd8
Aaron <aaron.bundock@cern.ch>
2019-03-28 11:08:47 +0100

    Use HGCAL Clusters

--- L1Trigger/L1CaloTrigger/python/L1TCaloEtSumProducer_cfi.py
+++ L1Trigger/L1CaloTrigger/python/L1TCaloEtSumProducer_cfi.py
@@ -5,10 +5,11 @@ L1TCaloEtSums = cms.EDProducer('L1TCaloEtSumProducer',
   ecalEtMin = cms.double(0),
   hcalDigis = cms.VInputTag(cms.InputTag('simHcalTriggerPrimitiveDigis')),
   hgCalColl = cms.InputTag("hgcalTriggerPrimitiveDigiProducer", "tower"),
+  hgCalMClusts = cms.InputTag("hgcalTriggerPrimitiveDigiProducer","cluster3D"),
   metEtaMax = cms.int32(28),
   metEtaMaxHF = cms.int32(41),
   ettEtaMax = cms.int32(28),
   ettEtaMaxHF = cms.int32(41),
   nTowThresholdHw = cms.int32(0),
-  nTowEtaMax = cms.int32(41)
+  nTowEtaMax = cms.int32(41),
 )
