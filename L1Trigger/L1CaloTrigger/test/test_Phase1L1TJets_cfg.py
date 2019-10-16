64e7f23c14993e6cd47c241435ad5cf064d517d4
Aaron Bundock <aaron.bundock@cern.ch>
2019-10-16 12:20:59 +0200

    Update test ph1 jets

--- L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
+++ L1Trigger/L1CaloTrigger/test/test_Phase1L1TJets_cfg.py
@@ -1,19 +1,40 @@
 import FWCore.ParameterSet.Config as cms
 from math import pi
 import FWCore.Utilities.FileUtils as FileUtils # ADDED
+import FWCore.ParameterSet.VarParsing as VarParsing # ADDED
+
 
 process = cms.Process("TEST")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+options = VarParsing.VarParsing ('analysis')
+# get and parse the command line arguments
 
-fileList = FileUtils.loadListFromFile('ttbar.list')
+options.register('skipEvents',
+                 0,
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.int,
+                 "Number of events to skip")
+options.register('outFile',
+                 'L1Ntuple.root',
+                  VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 'Output file')
+
+options.parseArguments()
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
+
+fileList = FileUtils.loadListFromFile('ttqcd.list')
 readFiles = cms.untracked.vstring(*fileList)
 
 process.source = process.source = cms.Source("PoolSource",
-  fileNames = readFiles,
+   skipEvents = cms.untracked.uint32(options.skipEvents), #added
+   fileNames = readFiles,
   #fileNames = cms.untracked.vstring(
   #  "file:pf500.root",
   #)
@@ -23,24 +44,25 @@ process.load('L1Trigger.L1CaloTrigger.Phase1L1TJets_cff')
 
 process.load('L1Trigger.Phase2L1ParticleFlow.l1pfJetMet_cff')
 
-process.load("L1Trigger.L1TNtuples.l1PhaseIPFJetTreeProducer_cfi")
-
-process.out = cms.OutputModule("PoolOutputModule",
-  fileName = cms.untracked.string('myOutputFile.root'),
-  outputCommands = cms.untracked.vstring(
-    "drop *",
-    "keep *_Phase1L1TJetProducer_*_*",
-    "keep *_ak4GenJetsNoNu_*_*",
-    "keep *_Phase1L1TJetCalibrator_*_*",
-    "keep *_*_*_*"
-  ),
-)
-
-#process.TFileService = cms.Service("TFileService",
-#    fileName = cms.string('L1Ntuple.root')
+#process.out = cms.OutputModule("PoolOutputModule",
+#  fileName = cms.untracked.string('myOutputFile.root'),
+#  outputCommands = cms.untracked.vstring(
+#    "drop *",
+#    "keep *_Phase1L1TJetProducer_*_*",
+#    "keep *_ak4GenJetsNoNu_*_*",
+#    "keep *_Phase1L1TJetCalibrator_*_*",
+#    "keep *_*_*_*",
+#  ),
 #)
 
+process.load("L1Trigger.L1TNtuples.l1PhaseIPFJetTreeProducer_cfi")
 
-process.p = cms.Path(process.Phase1L1TJetsSequence)
 
-process.e = cms.EndPath(process.out)
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('L1Ntuple.root')
+)
+
+
+process.p = cms.Path(process.Phase1L1TJetsSequence+process.l1PFJets)
+
+#process.e = cms.EndPath(process.out)
